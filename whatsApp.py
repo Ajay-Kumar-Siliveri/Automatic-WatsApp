daefc245110aa68d2ea2cade5a00103706ebdeab
Ajay Kumar Siliveri <65741738+Ajay-Kumar-Siliveri@users.noreply.github.com>
2021-05-07 13:22:22 +0530

    Update whatsApp.py

--- whatsApp.py
+++ whatsApp.py
@@ -1,3 +1,3 @@
 import pywhatkit as kit
 
-kit.sendwhatmsg("PH-Number","MESSAGE",12,9)#12:9 is the time to send message Automatically for given phone nymber
+kit.sendwhatmsg("PH-Number","MESSAGE",12,9)#12:9 is the time to send message Automatically for given phone number
